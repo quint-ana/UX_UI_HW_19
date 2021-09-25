1094cf2548367e9325edc715a80424d67f0e4f90
quint-ana <81986701+quint-ana@users.noreply.github.com>
2021-09-25 13:13:19 -0700

    fixed images

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+/* $("#button1").on("click", function(){
+    $(".myWork").animate("slow");
+}); */
+
+/* $(window).scroll(function() {
+    $('html, body').animate({
+        scrollTop: $("#button1").offset().top
+    }, 2000);
+}) */
